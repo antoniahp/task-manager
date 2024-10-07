53380d064d0fba127029a62b7e0f6ee82dddbd1a
antoniahp <a.herrerapuertas@gmail.com>
2024-10-07 11:52:39 +0200

    deleted: bool = Field(default=False) in schemas create and update

--- app/task_manager/infrastructure/task/create_task_schema.py
+++ app/task_manager/infrastructure/task/create_task_schema.py
@@ -1,13 +1,15 @@
 from typing import Optional
 from uuid import UUID
 from ninja import Schema
+from pydantic import Field
+
 
 class CreateTaskSchema(Schema):
     title: str
     description: str
     estimation: int
     completed: bool
-    deleted: bool
+    deleted: bool = Field(default=False)
     user_story_id: UUID
     sprint_id: Optional[UUID]
     assigned_user_id: Optional[UUID]
